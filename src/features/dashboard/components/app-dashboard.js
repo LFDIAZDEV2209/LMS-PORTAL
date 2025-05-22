75afbd4750362546587b81cc59c544aa7292026a
LeidyJohanaVillegas <lvillegas@gmail.com>
2025-05-21 21:16:03 -0500

    feat: :sparkles: agregada la funcionalidad de enrolled

--- src/features/dashboard/components/app-dashboard.js
+++ src/features/dashboard/components/app-dashboard.js
@@ -9,15 +9,15 @@ class DashboardCard extends HTMLElement {
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
           <div class="bg-white shadow-md rounded p-4 h-24 gap-6 ">
             <h2 class="text-xl font-semibold">Active Course</h2>
-            <p class="count text-[#3498d8]">3</p>
+            <p class="count text-[#3498d8] font-bold">3</p>
           </div>
           <div class="bg-white shadow-md rounded p-4 h-24 gap-6 ">
             <h2 class="text-xl font-semibold">Pending Assigments</h2>
-            <p class="count text-[#3498d8]">3</p>
+            <p class="count text-[#3498d8] font-bold">2</p>
           </div>
           <div class="bg-white shadow-md rounded p-4 h-24 gap-6">
             <h2 class="text-xl font-semibold">Overrall progress</h2>
-            <p class="count text-[#3498d8]">75%</p>
+            <p class="count text-[#3498d8] font-bold">75%</p>
           </div>
       </div>
     `;
--- src/features/dashboard/components/app-recentActivity.js
+++ src/features/dashboard/components/app-recentActivity.js
@@ -8,25 +8,21 @@ export class RecentActivity extends HTMLElement{
             <div class="gap-8 p-5 content-center mt-6 justify-center md:justify-between bg-white shadow-md rounded">
                 <h2 class="text-lg">Recent Activity</h2>
                 <div class="flex p-4 bg-white shadow-md rounded mb-1">
-                    <div class="h-10 w-10 rounded-full bg-[#E1F0FA]">📚</div>
+                    <div class="h-10 w-10 rounded-full bg-[#E1F0FA] justify-center content-center pl-2">📚</div>
                     <div class="ml-3 overflow-hidden w-1/1">
                         <h5 class="truncate text-sm font-medium text-gray-900">Completed chapter 5 in Unity Game Development</h5>
                         <p class="truncate text-sm text-gray-500 dark:text-gray-400">2 hours ago</p>
                     </div>
                 </div>
                 <div class="flex p-4  bg-white shadow-md rounded mb-1">
-                    <div>
-                        <img class="h-10 w-10 rounded-full bg-[#E1F0FA]" />
-                    </div>
+                    <div class="h-10 w-10 rounded-full bg-[#E1F0FA] justify-center content-center pl-2">✍️</div>
                     <div class="ml-3 overflow-hidden  w-1/1">
                         <h5 class="truncate text-sm font-medium text-gray-900">Completed chapter 5 in Unity Game Development</h5>
                         <p class="truncate text-sm text-gray-500 dark:text-gray-400">2 hours ago</p>
                     </div>
                 </div>
                 <div class="flex p-4 bg-white shadow-md rounded">
-                    <div>
-                        <img class="h-10 w-10 rounded-full bg-[#E1F0FA]"/>
-                    </div>
+                    <div class="h-10 w-10 rounded-full bg-[#E1F0FA] justify-center content-center pl-2">🏆</div>
                     <div class="ml-3 overflow-hidden  w-1/1">
                         <h5 class="truncate text-sm font-medium text-gray-900">Completed chapter 5 in Unity Game Development</h5>
                         <p class="truncate text-sm text-gray-500 dark:text-gray-400">2 hours ago</p>
