67ecfe5407a424d9ec31058c41bb51f92299aad5
LeidyJohanaVillegas <lvillegas@gmail.com>
2025-05-24 11:57:18 -0500

    feat: :lipstick: estilo de las tarjetas en dashboard

--- src/features/dashboard/components/app-dashboard.js
+++ src/features/dashboard/components/app-dashboard.js
@@ -7,17 +7,17 @@ class DashboardCard extends HTMLElement {
   render() {
     this.innerHTML = `
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pt-7">
-          <div class="bg-[#FFFFFF] rounded-lg shadow p-6">
+          <div class="bg-[#FFFFFF] rounded-lg h-52 shadow p-6">
             <h2 class="text-[35px] font-sans text-[#2C3E50]">Active Course</h2>
-            <p class="text-blue-500 text-[38px] font-bold">3</p>
+            <p class="text-blue-500 text-[38px] pt-7 font-bold">3</p>
           </div>
-          <div class="bg-[#FFFFFF] rounded-lg shadow p-6">
+          <div class="bg-[#FFFFFF] rounded-lg h-52 shadow p-6">
             <h2 class="text-[35px] font-sans text-[#2C3E50]">Pending Assigments</h2>
-            <p class="text-blue-500 text-[38px] font-bold">2</p>
+            <p class="text-blue-500 text-[38px] pt-7 font-bold">2</p>
           </div>
-          <div class="bg-[#FFFFFF] rounded-lg shadow p-6">
+          <div class="bg-[#FFFFFF] rounded-lg h-52 shadow p-6">
             <h2 class="text-[35px] font-sans text-[#2C3E50]">Overrall progress</h2>
-            <p class="text-blue-500 text-[38px] font-bold">75%</p>
+            <p class="text-blue-500 text-[38px] pt-7 font-bold">75%</p>
           </div>
       </div>
     `;
--- src/features/dashboard/components/app-enrolled.js
+++ src/features/dashboard/components/app-enrolled.js
@@ -19,28 +19,29 @@ export class AppEnrolled extends HTMLElement {
                 <div class="gap-8 mt-6 md:flex md:justify-between">
                     <h3 class="text-2xl content-center">My Enrolled Courses</h3>
                     <div class="flex justify-center py-1 pt-2">
-                        <a href="/courses-view" data link class="bg-[#3498db] hover:bg-blue-400 text-white font-bold py-3 px-3 rounded shadow-lg transform transition-transform duration-900 hover:-translate-y-0.5 hover:shadow-xl w-full">Browse More Courses</a>
+                        <a href="/courses-view" data link class="bg-[#3498db] text-center hover:bg-blue-400 text-white font-bold py-3 px-3 rounded shadow-lg transform transition-transform duration-900 hover:-translate-y-0.5 hover:shadow-xl w-full">Browse More Courses</a>
                     </div>
                 </div>
                 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-6 rounded">
                     ${filteredCourse.map(course => {
                         return `
-                        <div class="bg-white shadow-md rounded-2 transform h-[360px] rounded-lg transition-transform duration-300 hover:-translate-y-2 hover:shadow-xl">
+                        <div class="bg-white shadow-md rounded-2 transform h-[380px] rounded-lg transition-transform duration-300 hover:-translate-y-2 hover:shadow-xl">
                             <img src="${course.imageUrl}" class="w-full h-[180px] object-cover rounded-t-lg"/>
                             <div class="w-full h-1 bg-gray-200 rounded">
                                 <div class="h-full w-[50%] bg-green-500 rounded-r-lg"></div>
                             </div>
-                            <div class="p-6">
-                                <div class="py-1 gap-2"> 
-                                    <h4 class="font-semibold text-lg">${course.title}</h4>
-                                    <p>${course.instructor}</p>
+                            <div class="px-6 pt-3">
+                                <div class="py-1 gap-4"> 
+                                    <h4 class="font-semibold text-base font-sans">${course.title}</h4>
+                                    <p  class="opacity-50 pb-2 text-sm">${course.instructor}</p>
                                 </div>
-                                <div class="flex justify-between">
-                                    <span>65% complete</span>
+                                <hr>
+                                <div class="flex justify-between py-4">
+                                    <span class="text-green-400">65% complete</span>
                                     <span>13/24 Lessons</span>
                                 </div>
-                                <div class="flex justify-center pt-2">
-                                    <a href="/courses-details?id=${course.id}" data-link class="bg-[#3498db] hover:bg-blue-400 text-white font-bold py-3 px-3 rounded w-42">Continue Learning</a>
+                                <div class="flex justify-center w-full">
+                                    <a href="/courses-details?id=${course.id}" data-link class="bg-[#3498db] hover:bg-blue-400 text-white font-bold py-3 px-3 rounded w-full text-center">Continue Learning</a>
                                 </div>
                             </div>
                         </div>
