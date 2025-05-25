708dd65ac192dc883e28e48a2fa9f0a4976ccf32
LFDIAZDEV2209 <201288669+LFDIAZDEV2209@users.noreply.github.com>
2025-05-25 13:40:36 -0500

    feat: :sparkles: Cambiando url por url del json server en la nube

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_URL = import.meta.env.VITE_API_URL || 'https://server-lms-elgm.onrender.com';
 const myHeaders = new Headers({
     'Content-Type': 'application/json',
 });
