9ea57da47e9be0b5d87129682f467db0e2fc9e50
jcristancho2 <jocristanchool@gmail.com>
2025-05-26 11:15:13 -0500

    fix: :bug: Intentando otra vez

--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -9,9 +9,9 @@ class AppLayout extends HTMLElement {
 
     render() {
         this.innerHTML = `
-            <div class="min-h-screen flex flex-col box-border w-auto">
+            <div class="min-h-screen flex flex-col box-border">
                 <nav-bar class="sticky top-0 z-50 w-full"></nav-bar>
-                <main id="app" class="flex-1 container mx-auto w-auto " > <!-- modificacion para centrar el conendor del seccion  -->
+                <main id="app" class="flex-1 container mx-auto px-4" > <!-- modificacion para centrar el conendor del seccion  -->
 
                 </main>
             </div>
